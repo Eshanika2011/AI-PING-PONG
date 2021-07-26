6156e697a96b2e7f6c0dc48945353d1be87ff760
Eshanika2011 <75015294+Eshanika2011@users.noreply.github.com>
2021-07-26 18:27:29 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -20,10 +20,36 @@ var ball = {
     dx:3,
     dy:3
 }
+img = "";
+noseX = 0;
+noseY = 0;
+marioX = 325;
+marioY = 325;
+
+function preload() {
+  
+}
+
 
 function setup(){
   var canvas =  createCanvas(700,600);
+  video = createCapture(VIDEO);
+  video.size(600, 300);
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on("pose", gotPoses);
 }
+function modelLoaded() {
+  console.log("Model Loaded!");
+
+}
+function gotPoses(results) {
+  if (results.length > 0) {
+    noseX = results[0].pose.nose.x;
+    noseY = results[0].pose.nose.y;
+    console.log("noseX = " + noseX + ", noseY = " + noseY);
+  }
+}
+
 
 
 function draw(){
